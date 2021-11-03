d6ea2cfa88682ce83cfaa9a17606f673f34dae69
nise_nabe <nise.nabe@gmail.com>
2021-11-03 17:22:39 +0900

    fix to use java11 for java compile

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,6 +49,11 @@ tasks {
         changeNotes.set(provider { changelog.getLatest().toHTML()} )
     }
 
+    withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_11.toString()
+        targetCompatibility = JavaVersion.VERSION_11.toString()
+    }
+
     withType<KotlinCompile>().configureEach {
         kotlinOptions {
             targetCompatibility = JavaVersion.VERSION_11.toString()
