ac1d7fd7526d7f81603f8030497cc70a2ba7cf01
nise_nabe <nise.nabe@gmail.com>
2021-11-03 15:19:26 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,66 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm")
+    id("org.jetbrains.intellij")
+    id("org.jetbrains.changelog")
+    id("org.jetbrains.gradle.plugin.idea-ext")
+    id("com.nisecoder.idea-ext-ext")
+    id("org.asciidoctor.jvm.convert")
+}
+
+group = "com.nisecoder.intellij"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+}
+
+idea {
+    module {
+        settings {
+            packagePrefix["src/main/kotlin"] = "com.nisecoder.intellij.plugins.atcoder"
+        }
+    }
+}
+
+// See https://github.com/JetBrains/gradle-intellij-plugin/
+intellij {
+    version.set("2021.2.3")
+    type.set("IU")
+    downloadSources.set(true)
+    plugins.set(listOf("org.jetbrains.plugins.gradle"))
+}
+
+tasks {
+    runIde {
+        autoReloadPlugins.set(true)
+    }
+    val changelog: ChangelogPluginExtension = extensions.getByType()
+    patchPluginXml {
+        changeNotes.set(provider { changelog.getLatest().toHTML()} )
+    }
+
+    asciidoctor {
+        // collect into root buildDirectory for publishing to GitHub Pages
+        val outputDir = if (project == rootProject) {
+            rootProject.buildDir.resolve("docs/asciidoc")
+        } else {
+            rootProject.buildDir.resolve("docs/asciidoc/${project.name}")
+        }
+        setOutputDir(outputDir)
+    }
+
+    withType<KotlinCompile>().configureEach {
+        kotlinOptions {
+            targetCompatibility = JavaVersion.VERSION_11.toString()
+            apiVersion = "1.5"
+            languageVersion = "1.5"
+            javaParameters = true
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+rootProject.name = "atcoder-intellij-plugin"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        exclusiveContent {
+            forRepository {
+                maven {
+                    name = "gitHubPackages"
+                    url = uri("https://maven.pkg.github.com/nise-nabe/gradle-plugins")
+                    credentials(PasswordCredentials::class)
+                }
+            }
+            filter {
+                includeGroup("com.nisecoder.gradle.plugin")
+            }
+        }
+    }
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id.startsWith("com.nisecoder.idea-ext-ext")) {
+                useModule("com.nisecoder.gradle.plugin:idea-ext-ext:0.0.1")
+            }
+        }
+    }
+
+    plugins {
+        kotlin("jvm") version "1.5.31"
+        id("org.jetbrains.intellij") version "1.2.1"
+        id("org.jetbrains.gradle.plugin.idea-ext") version "1.1"
+        id("org.asciidoctor.jvm.convert") version "3.+"
+        id("org.jetbrains.changelog") version "1.3.1"
+    }
+}
