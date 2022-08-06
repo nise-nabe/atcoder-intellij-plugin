374ba9a12f734c8774b9aba698cf918379357079
nise_nabe <nise.nabe@gmail.com>
2022-08-06 16:17:54 +0900

    to be simple

--- src/main/kotlin/AtCoderWindowFactory.kt
+++ src/main/kotlin/AtCoderWindowFactory.kt
@@ -1,27 +1,16 @@
 package com.nisecoder.intellij.plugins.atcoder
 
-import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
-import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.ui.content.ContentFactory
 import com.intellij.ui.jcef.JBCefBrowser
-import javax.swing.JComponent
 
 class AtCoderWindowFactory: ToolWindowFactory {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val service = project.service<AtCoderWindowService>()
-        toolWindow.component.parent.add(service.window.content)
+        val browser = JBCefBrowser("https://atcoder.jp")
+        val content = ContentFactory.getInstance().createContent(browser.component, "", false)
+
+        toolWindow.contentManager.addContent(content)
     }
-}
-
-class AtCoderWindowService(project: Project) {
-    val window: AtCoderWindow = AtCoderWindow(project)
-}
-
-class AtCoderWindow(project: Project) {
-    val content: JComponent = JBCefBrowser().apply {
-        loadURL("https://atcoder.jp")
-        Disposer.register(project, this)
-    }.component
 }
